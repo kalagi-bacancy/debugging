801be63d03f1373538abdc19e1973167ee523fdf
Kalagi Vora <kalagi.vora@bacancy.com>
2024-03-27 18:03:57 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'error-handling';
+  user = {};
+  employees: any;
+
+  addEmp(): void{
+    this.employees.push('employee')
+  }
+
+  onRemoveEmp(id: number) {
+    const position = id + 1;
+    this.employees.splice(position, 1);
+  }
 }
